4e4f523e48b679c8b40dbb5469d97e716215cfa9
Christian Norrman <christian@faxity.se>
2019-02-09 16:33:07 +0100

    Fixed SPA routing

--- src/app.js
+++ src/app.js
@@ -9,13 +9,45 @@ import Layout from './Layout.vue';
 import routes from './routes';
 import MDCDataTable from './components/MdcTable';
 
+// Define plugins
 Vue.use(VueRouter);
 Vue.use(VueMDC);
 Vue.use(MDCDataTable);
 
+// Create router instance
 const Router = new VueRouter({
   mode: 'history',
   routes,
+
+  // Title plugin
+  /*beforeEnter(to, from, next) {
+    console.log(to);
+    console.log(from);
+
+    debugger;
+    const match = to.matched.find(record => record.meta.title);
+    if(match && match.meta && match.meta.title) {
+      document.title = match.meta.title;
+    }
+    next();
+  },*/
+});
+
+// This is needed for github to allow history mode
+Router.beforeEach((to, from, next) => {
+  const page = to.query.p;
+  if (page) {
+    console.log(page);
+    return next(page);
+  }
+
+  next();
+});
+Router.afterEach((to, from) => {
+  const match = to.matched.find(record => record.meta.title);
+  if(match && match.meta && match.meta.title) {
+    document.title = match.meta.title;
+  }
 });
 
 // Create vue instance
@@ -23,15 +55,6 @@ new Vue({
   el: '#app',
   router: Router,
   render: h => h(Layout),
-
-  // Title plugin
-  beforeEnter(to, from, next) {
-    const match = to.matched.find(record => record.meta.title);
-    if(match && match.meta && match.meta.title) {
-      document.title = match.meta.title;
-    }
-    next();
-  },
 });
 
 module.hot && module.hot.accept();
